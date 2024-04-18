4c2288459d9d438181c5c8507c082650cc115d7e
Khánh <nguyen.duc@ascendcorp.com>
2024-04-18 13:30:02 +0700

    change forder

--- src/main/java/com/example/final_case_social_web/config/AppContextConfiguration.java
+++ src/main/java/com/example/final_case_social_web/config/AppContextConfiguration.java
@@ -1,5 +1,6 @@
-package com.example.final_case_social_web.component;
+package com.example.final_case_social_web.config;
 
+import com.example.final_case_social_web.component.RequestLimitingInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.handler.MappedInterceptor;
